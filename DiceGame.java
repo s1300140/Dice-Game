a2f3edacb5bb1b7f01d1699f47797cb6c6f16fc2
s1300140 <s1300140@u-aizu.ac.jp>
2023-06-09 18:24:29 +0900

    test

--- DiceGame.java
+++ DiceGame.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 import java.util.Random;
 class DiceGame{
     public static void main(String[] args)
     {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("What is your name?");
+	String name=sc.nextLine();
+	System.out.println("Hello, "+name+"!");
 	Random rand=new Random();
 	System.out.println("Rolling dice...");
 	int dice1 = rand.nextInt(6) + 1;
@@ -10,6 +15,10 @@ class DiceGame{
 	System.out.println("Die 2: "+dice2);
 	int sum=dice1+dice2;
 	System.out.println("Total value: "+sum);
+	if(sum>=7)
+	    System.out.println(name+" won!");
+	else
+	    System.out.println(name+" loss");
     }
 }
 		
